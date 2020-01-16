9c335148be499d7faea5a71906047cfdba740cda
Sashant <sashant@Sashants-MacBook-Pro.local>
2020-01-16 21:16:47 +0530

    changes to image.tf and variable.tf

--- variable.tf
+++ variable.tf
@@ -45,3 +45,21 @@ variable "zone" {
   default     = "us-south-1"
   description = "The VPC Zone that you want your VPC networks and virtual servers to be provisioned in. To list available zones, run `ibmcloud is zones`."
 }
+
+variable "ibmcloud_svc_api_key" {
+  default     = ""
+  type        = "string"
+  description = "The APIKey of the IBM Cloud service account that is hosting the F5-BIGIP qcow2 image file. This should be a the API Key of a Service ID in the account"
+}
+variable "vnf_cos_image_url" {
+  default     = ""
+  description = "The COS image object SQL URL for F5-BIGIP qcow2 image."
+}
+variable "vnf_cos_instance_id" {
+  default     = ""
+  description = "The COS instance-id hosting the F5-BIGIP qcow2 image."
+}
+variable "vnf_vpc_image_name" {
+  default     = "checkpoint-sakthi-test1"
+  description = "The name of the F5-BIGIP custom image to be provisioned in your IBM Cloud account."
+}
