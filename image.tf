829ba8b9afddc5f895b3c3c765f6b1cf936f9eec
Sashant <sashant@sashants-mbp.in.ibm.com>
2020-01-16 12:16:34 +0530

    added image.tf

---
+++ image.tf
@@ -0,0 +1,8 @@
+module “f5_vnf_image” {
+  source = “git::https://github.com/sakthishanmugam02/vnf-on-vpc-modules.git”
+  ibmcloud_endpoint    = “”
+  ibmcloud_svc_api_key = “”
+  vnf_cos_image_url    = “”
+  vnf_cos_instance_id  = “”
+  vnf_vpc_image_name   = “”
+}
