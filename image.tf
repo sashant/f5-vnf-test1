82abf26fce3101a9ee657d7a0ccc820a94cfd27f
Sashant <sashant@sashants-mbp.in.ibm.com>
2020-01-16 12:53:46 +0530

    changes to image.tf

--- image.tf
+++ image.tf
@@ -1,8 +1,11 @@
 module "f5_vnf_image" {
   source = "git::https://github.com/sakthishanmugam02/vnf-on-vpc-modules.git"
-  ibmcloud_endpoint    = ""
-  ibmcloud_svc_api_key = ""
-  vnf_cos_image_url    = ""
-  vnf_cos_instance_id  = ""
-  vnf_vpc_image_name   = ""
+  ibmcloud_endpoint    = "test.cloud.ibm.com"
+  ibmcloud_svc_api_key = "6ob7CwRoZBHnrz3JKZkF5wJ_JRsHaF2F1dxIV72-SMdq"
+  vnf_cos_image_url    = "cos://us-south/vnfimagesstage/BIGIP-15.0.1-0.0.11.qcow2"
+  vnf_cos_instance_id  = "3fae3104-d6cc-4c15-94db-2635e2cc50b9"
+  vnf_vpc_image_name   = "f5-bigip-sashant-test1"
+  vpc_name             = "sashant-vpc"
+  resource_group       = "Default"
+  region               = "us-south"
 }
